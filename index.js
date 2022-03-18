2e24a4222a6c920ed9cd30d585af43793da3389b
LinaPeralta <69905738+LinaPeralta@users.noreply.github.com>
2022-03-18 10:11:09 -0500

    for

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 
-
+//punto 1
+//pintar el username en la web
 const user__name = document.getElementById("user__name");
 
+//objeto user
 const user = {
     username: "Lina",
     age: 19,
@@ -9,11 +11,14 @@ const user = {
     email: "peraltalinam13@gmail.com"
 };
 
+//leer el objeto user en la consola
 console.log("Información de usuario:" + "Nombre: " + user.username + ", " + "Edad: " + user.age + ", " + "Género: " + user.gender + ", " + "Correo: " + user.email);
 
+//modificar el h4 del html con la info del js
 user__name.innerHTML = "¡Bienvenida "+ user.username + "!";
 
-
+//punto 2
+//arreglo de productos
 const products = [
 {
     name: "Cheesecake de moras",
@@ -27,7 +32,7 @@ const products = [
     price: 32000,
     description: "x8 porciones",
     categorie: "cakes",
-    stock: 4,
+    stock: 2,
 },
 {
     name: "Torta de chocolate",
@@ -48,7 +53,7 @@ const products = [
     price: 16000,
     description: "x4",
     categorie: "desserts",
-    stock: 8,
+    stock: 0,
 },
 {
     name: "Alfajores de chocolate",
@@ -59,4 +64,24 @@ const products = [
 }
 ];
 
-console.log(products[0]);
+//punto 3
+//condicionales: for para recorrer el arreglo de productos y  e if encontrar los que tienen prceio igual o mayor a 30000
+
+console.log("Productos con precio igual o mayor a 30000: "); 
+  for (let index = 0; index < products.length; index++) {
+      if (products[index].price >= 30000) {
+          console.log(products[index].name);
+      }
+  }
+
+  //productos sin stock
+  console.log("Productos sin stock: "); 
+  for (let index = 0; index < products.length; index++) {
+      if (products[index].stock <= 0) {
+          console.log(products[index].name);
+      }
+  }
+
+  
+
+  
