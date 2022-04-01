5000c3a4c91f1ab341c324300c25128af6a5c191
LinaPeralta <69905738+LinaPeralta@users.noreply.github.com>
2022-03-31 19:13:22 -0500

    addEventListener

--- DOMex/DOM.js
+++ DOMex/DOM.js
@@ -1,16 +1,27 @@
-const form = document.getElementById("submit");
+const form = document.getElementById("forms");
 
 const itemImage = document.getElementById("form__item__img");
 const itemName = document.getElementById("form__item__name");
 const itemDescription = document.getElementById("form__item__description");
 const itemPrice = document.getElementById("form__item__price");
+const itemStock = document.getElementById("form__item__Stock");
 const itemCategorie = document.getElementById("item__dropDownList");
+const itemSubmit = document.getElementById("form__item__Btn");
 
+form.addEventListener("submit",  ev =>{
+ev.preventDefault;
 
-form.addEventListener("submit",  e=>{
-e.preventDefault;
+if(itemName.value === '' || itemDescription.value ==='' || itemPrice.value === ''){
+    alert("Hay campos sin llenar. Por favor llena los campos");
+} else { 
 
-console.log(form.itemName, form.itemPrice, form.itemDescription);
+    const products = {
+        name: itemName.value,
+        description: itemDescription.value,
+        price: itemPrice.value,
+        stock: itemStock.value
+    }
+}
 
-})
+});
 
