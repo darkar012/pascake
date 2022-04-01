77a032e0d7ba8cd98275400b0af62d68c4a2956b
Lina Maria Peralta Delgado <1006052110@u.icesi.edu.co>
2022-04-01 08:49:17 -0500

    image galery

--- DOMex/DOM.js
+++ DOMex/DOM.js
@@ -1,15 +1,15 @@
 const form = document.getElementById("forms");
 
-const itemImage = document.getElementById("form__item__img");
-const itemName = document.getElementById("form__item__name");
-const itemDescription = document.getElementById("form__item__description");
-const itemPrice = document.getElementById("form__item__price");
-const itemStock = document.getElementById("form__item__Stock");
-const itemCategorie = document.getElementById("item__dropDownList");
-const itemSubmit = document.getElementById("form__item__Btn");
+const itemImage = form.img;
+const itemName = form.name;
+const itemDescription = form.description;
+const itemPrice = form.price;
+const itemStock = form.stock;
+const itemCategory = form.category;
+const itemSubmit = form.btn;
 
 form.addEventListener("submit",  ev =>{
-ev.preventDefault;
+ev.preventDefault();
 
 if(itemName.value === '' || itemDescription.value ==='' || itemPrice.value === ''){
     alert("Hay campos sin llenar. Por favor llena los campos");
@@ -21,6 +21,8 @@ if(itemName.value === '' || itemDescription.value ==='' || itemPrice.value === '
         price: itemPrice.value,
         stock: itemStock.value
     }
+
+    console.log(products);
 }
 
 });
