e4b38672234bc41e5a90318969ba92d84c5a744e
LinaPeralta <69905738+LinaPeralta@users.noreply.github.com>
2022-04-23 20:12:09 -0500

    xd

--- src/signup.js
+++ src/signup.js
@@ -1,64 +1,61 @@
 
 import { async } from "@firebase/util";
-import { getdb ,getApp, initializeApp } from "firebase/app";
-import { getAuth, createUserWithEmailAndPassword} from "firebase/auth";
+import { getdb, getApp, initializeApp } from "firebase/app";
+import { getAuth, createUserWithEmailAndPassword } from "firebase/auth";
 //import {getFirestore, doc, adDoc} from "firebase/firestore";
 import { doc, getFirestore, setDoc } from "firebase/firestore";
 
 
+// Initialize Firebase
 
+//const app = initializeApp(firebaseConfig);
+const auth = getAuth();
+const db = getFirestore(getApp());
+console.log(db);
+const createUserForm = document.getElementById("singUpForm");
 
- // Initialize Firebase
+createUserForm.addEventListener("submit", async (e) => {
+    e.preventDefault();
 
- //const app = initializeApp(firebaseConfig);
- const auth = getAuth();
- console.log(getdb());
+    const name = createUserForm.name.value;
+    const lastname = createUserForm.lastname.value;
+    const email = createUserForm.email.value;
+    const password = createUserForm.password.value;
 
+    const userInfo = {
+        name,
+        lastname,
+        email,
+        password
+    };
 
- const createUserForm = document.getElementById("singUpForm");
+    const newUser = await createUser(auth, userInfo.email, userInfo.password);
+    await addUserToDatabase(db, newUser.uid, userInfo);
 
- createUserForm.addEventListener("submit", async (e) => {
- e.preventDefault();
+    createUser(name, lastname, email, password);
 
- const name = createUserForm.name.value;
- const lastname = createUserForm.lastname.value;
- const email = createUserForm. email.value;
- const password = createUserForm.password.value;
+});
 
-  const userInfo = {
-    name,
-    lastname,
-    email,
-    password
- };
+async function createUser(email, password) {
+    try {
+        const { user } = await createUserWithEmailAndPassword(auth, email, password);
+        alert(`Bienvenido, usuario ${user.email}`);
+    } catch (e) {
 
- const newUser = await createUser(auth, userInfo.email, userInfo.password);
- await addUserToDatabase(db, newUser.uid, userInfo);
-
-  createUser(name,lastname,email, password);
-
- });
-
-    async function createUser(email, password) {
-        try {
-            const { user } = await createUserWithEmailAndPassword(auth, email, password);
-            alert(`Bienvenido, usuario ${user.email}`);
-        } catch(e) {
-     
-         if(e.code === "auth/weak-password") {
-             alert("Tu contraseña debe tener al menos 6 caracteres");
-         }
-     
-         if(e.code === "auth/email-already-in-use") {
-             alert("Este correo ya se encuentra en uso");
-         }
+        if (e.code === "auth/weak-password") {
+            alert("Tu contraseña debe tener al menos 6 caracteres");
         }
-     }
 
-     async function addUserToDatabase(db, userId, userInfo = {}){
-         try {
-             await setDoc (doc(db, "users", userId), userInfo);
-         } catch (e){
-             console.log(e);
-         }
-     }
+        if (e.code === "auth/email-already-in-use") {
+            alert("Este correo ya se encuentra en uso");
+        }
+    }
+}
+
+async function addUserToDatabase(db, userId, userInfo = {}) {
+    try {
+        await setDoc(doc(db, "users", userId), userInfo);
+    } catch (e) {
+        console.log(e);
+    }
+}
