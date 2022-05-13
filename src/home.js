590714d582678a6d29a97808617816e3b9c68dcc
LinaPeralta <peraltalinam13@gmail.com>
2022-05-12 21:55:55 -0500

    trying to make work the add to cart button

--- src/home.js
+++ src/home.js
@@ -3,33 +3,38 @@ import { getProducts } from "./allProducts";
 import { getFirebaseCart, createFirebaseCart } from "../src/cart"
 
 const shopBakery = document.getElementById("bakery");
-const categoryFilter = document.getElementById("category"); 
+const categoryFilter = document.getElementById("category");
 
 let products = [];
+let cart = getMyCart();
+console.log(cart);
 
-async function loadProducts(){
-  const firebaseProducts =  await getProducts(db);
-  firebaseProducts.forEach(product =>{
-    renderProduct(product);
-});
-        products = firebaseProducts;                            
+
+async function loadProducts() {
+    const firebaseProducts = await getProducts(db);
+    firebaseProducts.forEach(product => {
+        renderProduct(product);
+    });
+    products = firebaseProducts;
 }
 
 
-function renderProduct(item){
+function renderProduct(item) {
     //console.log(item);
     const product = document.createElement("a");
 
-
-    product.className ="product"; 
-    product.setAttribute("href", `./product.html?id=${item.id}`);
-    const coverImage = item.img.length ? item.img[0] : "https://cdn.dribbble.com/users/55871/screenshots/2158022/media/95f08ed3812af28b93fa534fb5d277b3.jpg";
-    
-    //cart
     const isProductAddedToCart = cart.some((productCart) => productCart.id === item.id);
     const productButtonCart = isProductAddedToCart ?
-    '<button class="product__cart" disabled>Producto añadido</button>':
-    '<button class="product__cart">Añadir al carrito</button>';
+    '<button class="product_cart" disabled>Producto añadido</button >':
+    '<button class="product_cart">Añadir al carrito</button>';
+
+    console.log(isProductAddedToCart);
+
+
+    product.className = "product";
+    product.setAttribute("href", `./product.html?id=${item.id}`);
+    const coverImage = item.img.length ? item.img[0] : "https://cdn.dribbble.com/users/55871/screenshots/2158022/media/95f08ed3812af28b93fa534fb5d277b3.jpg";
+
 
     product.innerHTML = `
     
@@ -37,33 +42,53 @@ function renderProduct(item){
             <img src="${coverImage}" alt="" class="dessert__img">
             <h3 class="dessert__caption">${item.name}</h3>
             <p class="dessert__description">$${item.price} ${item.description}</p>
-             ${productButtonCart}
+            ${productButtonCart}
         </div>`;
- 
+
     shopBakery.appendChild(product);
-                                
+
+    const productCartButton = product.querySelector(".product__cart");
+    //console.log(productCartButton);
+
+    productCartButton.addEventListener("click", e => {
+        e.preventDefault();
+
+        cart.push(item);
+        addProductToCart();
+        productCartButton.setAttribute("disabled", true);
+
+    });
+
 }
 
+function addProductToCart() {
+    localStorage.setItem("cart", JSON.stringify(cart));
+}
 
-function filterBy(){
-    
+function getMyCart() {
+    const myCart = localStorage.getItem("cart");
+    return myCart ? JSON.parse(myCart) : [];
+}
+
+function filterBy() {
+
     const newCategory = categoryFilter.value;
-    let filteredProducts =[];
+    let filteredProducts = [];
 
     if (newCategory !== "") {
-         filteredProducts = products.filter((product) => product.category === newCategory); 
+        filteredProducts = products.filter((product) => product.category === newCategory);
     } else {
         filteredProducts = products;
     }
 
-    shopBakery.innerHTML=""; 
-    filteredProducts.forEach(product=> {
+    shopBakery.innerHTML = "";
+    filteredProducts.forEach(product => {
         renderProduct(product);
     });
-    
+
 }
 
-categoryFilter.addEventListener("change",e=>{
+categoryFilter.addEventListener("change", e => {
     filterBy();
 });
 
